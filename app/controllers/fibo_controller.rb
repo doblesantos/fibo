2b1b5e57a25708169e594bef31b33b2be0157e34
Freddy <doblesantos91@gmail.com>
2021-01-31 10:11:11 -0300

    calculate fibonnaci

---
+++ app/controllers/fibo_controller.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+class FiboController < ApplicationController
+  def caculate
+    index = params.require(:index)
+
+    secuense = fib(index)
+
+    render json: secuense[index]
+  end
+
+  private
+
+  def fib(n)
+    secuense = [0, 1]
+
+    (n - 1).times do
+      size = secuense.size
+      secuense.push(secuense[(size - 2)] + secuense[(size - 1)])
+    end
+    secuense
+  end
+end
